bcc590d020a0df4f1e8f2baae983d13bbbcd16c4
Dmitry Zudochkin <dnd.pliz@gmail.com>
2013-05-15 21:11:15 +0400

    added rabl template; fixed bug with render tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+
+    respond_to do |format|
+      format.json { render }
+    end
+  end
 end
---
+++ app/views/api/v1/tasks/index.rabl
@@ -0,0 +1,2 @@
+collection @tasks
+attributes :id, :title, :description, :completed_at
