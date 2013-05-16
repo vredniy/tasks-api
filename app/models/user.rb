03fff1ccacac7341764fc6a0184c3a5cf15031be
Dmitry Zudochkin <dnd.pliz@gmail.com>
2013-05-16 21:47:51 +0400

    [1-authentication] added Devise gem; trying to sign_in via curl out failed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  devise :token_authenticatable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
   has_many :tasks
-  attr_accessible :name
+
+  before_save :ensure_authentication_token
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 TasksApi::Application.routes.draw do
+
   namespace :api do
     namespace :v1 do
+      #devise_scope :user do
+        #post 'sessions' => 'sessions#create', as: 'login'
+        #delete 'sessions' => 'sessions#destroy', as: 'logout'
+      #end
+      devise_for :users
+
       resources :tasks
     end
   end
